ce38e4f8801335a53f531ec51dd03967269f4358
Adedokun Toheeb Adetunji <63404340+RxNode@users.noreply.github.com>
2022-02-07 19:36:32 +0100

    laundry exercise solution

--- src/laundry.js
+++ src/laundry.js
@@ -7,6 +7,39 @@
  *
  * @returns {number}
  */
-function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {}
-
+ function getMaxPairs(noOfWashes, cleanPile, dirtyPile) {
+  let maxSockPair = 0;
+  let singleSock = [];
+  cleanPile = cleanPile.sort((a, b) => {
+    return a - b;
+  })
+  dirtypile = dirtyPile.sort((a, b) => {
+    return a - b;
+  })
+  for (let x = 0; x < cleanPile.length; x++) {
+    if (cleanPile[x] === cleanPile[x + 1]) {
+      maxSockPair++
+      cleanPile.splice(x, 1
+        )
+    }
+    else {
+      singleSock.push(cleanPile[x]);
+    }
+  }
+  for (let y = 0; y < singleSock.length; y++) {
+    if (dirtyPile.includes(singleSock[y]) && noOfWashes > 0) {
+      dirtyPile.splice(y, 1);
+      maxSockPair++
+      noOfWashes--
+    }
+  }
+  for (let z = 0; z < dirtyPile.length; z++) {
+    if (dirtyPile[z] === dirtyPile[z + 1] && noOfWashes > 1) {
+      maxSockPair++
+      noOfWashes -= 2
+      dirtyPile.splice(z, 1);
+    }
+  }
+  return maxSockPair;
+}
 module.exports = getMaxPairs;
